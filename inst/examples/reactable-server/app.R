e8cf20f4e2b53a293117840af4c133176ab0cbb2
Recle E. Vibal <recle.vibal@appsilon.com>
2023-04-21 22:50:54 +0800

    Feat: Adds example of reactable server

---
+++ inst/examples/reactable-server/app.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(reactable.extras)
+
+shinyApp(
+  reactableExtrasUi("test"),
+  function(input, output, server){
+    reactableExtrasServer(
+      "test",
+      data = tibble::rownames_to_column(mtcars, var = "make"),
+      columns = list(
+        mpg = reactable::colDef(name = "Miles per Gallon"),
+        cyl = reactable::colDef(name = "Cylinders"),
+        disp = reactable::colDef(name = "Displacement")
+      ),
+      rows_per_page = 6
+    )
+  }
+)
