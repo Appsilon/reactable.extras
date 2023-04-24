6ef2038abf3d6a00422ea8191d2fad35a687e6da
Recle E. Vibal <recle.vibal@appsilon.com>
2023-04-24 16:19:29 +0800

    Chore: Removes text clutter

--- R/dependencies.R
+++ R/dependencies.R
@@ -6,10 +6,6 @@ reactableExtrasDependency <- function() {
   htmltools::htmlDependency(
     "reactable-server", "0.0.1",
     src = list(file = "assets/reactable-server"), package = "reactable.extras",
-    # NOTE: Updated version (v1.6.4) of bootstrap-datepicker currently not
-    #   used by base shiny. This version includes content and tooltip support
-    #   for the beforeShowDay function family. for more information on this
-    #   see https://bootstrap-datepicker.readthedocs.io/en/latest/options.html#beforeshowday
     script = "js/reactable-server.js",
     stylesheet = "css/reactable-server.css"
   )
