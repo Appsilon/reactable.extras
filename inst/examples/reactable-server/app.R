9d6f2d827ba22a8223eeaff41c349cc9291f5da6
Recle E. Vibal <recle.vibal@appsilon.com>
2023-04-24 21:42:05 +0800

    Feat: Updates example with bigger data

--- inst/examples/reactable-server/app.R
+++ inst/examples/reactable-server/app.R
@@ -1,18 +1,25 @@
 library(shiny)
 library(reactable.extras)
 
+mtcars_ultra <- purrr::map_dfr(
+  seq(1L, 20000L, by = 1L),
+  ~ mtcars |>
+    tibble::rownames_to_column("make") |>
+    dplyr::mutate(id_row = paste0("id_", dplyr::row_number(), "_", .x))
+)
+
 shinyApp(
   reactableExtrasUi("test"),
   function(input, output, server) {
     reactableExtrasServer(
       "test",
-      data = tibble::rownames_to_column(mtcars, var = "make"),
+      data = mtcars_ultra,
       columns = list(
         mpg = reactable::colDef(name = "Miles per Gallon"),
         cyl = reactable::colDef(name = "Cylinders"),
         disp = reactable::colDef(name = "Displacement")
       ),
-      rows_per_page = 6
+      rows_per_page = 17
     )
   }
 )
