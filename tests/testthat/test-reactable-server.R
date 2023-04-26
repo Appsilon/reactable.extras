0f15866b80951117521d6d4473434aefcd94e197
Recle E. Vibal <recle.vibal@appsilon.com>
2023-04-26 15:09:33 +0800

    Feat: Adds shinytest to reactable server module

--- tests/testthat/test-reactable-server.R
+++ tests/testthat/test-reactable-server.R
@@ -1,6 +1,5 @@
 library(shiny)
-library(dplyr)
-library(reactable)
+library(shinytest2)
 
 test_that("reactable_page_controls should return UI for page navigation and display", {
   expect_snapshot(reactable_page_controls("test"))
@@ -39,4 +38,54 @@ test_that("return_reactable_page should return a reactive page value", {
 
 test_that("reactableExtrasUI should return a widget of reactableOutput", {
   expect_snapshot(reactableExtrasUi("test"))
+})
+
+test_that("reactablExtrasServer should display the correct reactable page", {
+  skip_on_cran()
+
+  motor_trend_cars <- mtcars
+  motor_trend_cars$make <- rownames(motor_trend_cars)
+  rownames(motor_trend_cars) <- NULL
+
+  test_app <- shinyApp(
+    reactableExtrasUi("test"),
+    function(input, output, server) {
+      reactableExtrasServer(
+        "test",
+        data = motor_trend_cars,
+        columns = list(
+          mpg = reactable::colDef(name = "Miles per Gallon"),
+          cyl = reactable::colDef(name = "Cylinders"),
+          disp = reactable::colDef(name = "Displacement")
+        ),
+        rows_per_page = 17
+      )
+    }
+  )
+
+  app <- AppDriver$new(test_app, name = "test", screenshot_args = FALSE)
+
+  app$set_window_size(width = 1619, height = 1049)
+  app$expect_values()
+  app$click("test-page_controls-next_page")
+  app$expect_values()
+  app$click("test-page_controls-last_page")
+  app$expect_values()
+  app$click("test-page_controls-previous_page")
+  app$expect_values()
+  app$click("test-page_controls-first_page")
+  app$expect_values()
+  app$set_inputs(`test-reactable__reactable__sorted` = "asc", allow_no_input_binding_ = TRUE)
+  app$expect_values()
+  app$set_inputs(`test-reactable__reactable__sorted` = "desc", allow_no_input_binding_ = TRUE)
+  app$expect_values()
+  app$set_inputs(`test-reactable__reactable__sorted` = character(0), allow_no_input_binding_ = TRUE)
+  app$expect_values()
+  app$set_inputs(`test-reactable__reactable__sorted` = "asc", allow_no_input_binding_ = TRUE)
+  app$expect_values()
+  app$set_inputs(`test-reactable__reactable__sorted` = "desc", allow_no_input_binding_ = TRUE)
+  app$expect_values()
+  app$set_inputs(`test-reactable__reactable__sorted` = "asc", allow_no_input_binding_ = TRUE)
+  app$expect_values()
+
 })
