29321defb461295c47552e50b2c1c967dd4ff47f
Recle E. Vibal <recle.vibal@appsilon.com>
2023-04-27 15:08:42 +0800

    Chore: Reorganizes html dependency files
    Again, for more consistent organization in
    preparation for merge later

--- R/dependencies.R
+++ R/dependencies.R
@@ -5,7 +5,7 @@
 reactable_extras_dependency <- function() {
   htmltools::htmlDependency(
     "reactable-server", "0.0.1",
-    src = list(file = "assets/reactable-server"), package = "reactable.extras",
+    src = list(file = "assets"), package = "reactable.extras",
     script = "js/reactable-server.js",
     stylesheet = "css/reactable-server.css"
   )
