82ef2481ad0e67848f6602d0079df60467da4142
Ivan Hrychaniuk <vanhry@gmail.com>
2023-05-02 09:57:21 +0300

    update cypress tests based on new code

--- js/cypress/e2e/spec.cy.js
+++ js/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('Dropdown in reactable passes values to the shiny', () => {
   it('Dropdown', () => {
     cy.visit('http://localhost:8888');
     cy.get('.dropdown-extra').eq(1).select("Large").should('have.value', 'Large');
-    cy.contains('Dropdown: {row : 1, value : Large}');
+    cy.contains('Dropdown: {row : 2, value : Large, column : Type}');
   });
 })
 
@@ -12,7 +12,7 @@ describe('Checkbox in reactable passes values to the shiny', () => {
   it('Checkbox', () => {
     cy.visit('http://localhost:8888');
     cy.get('.checkbox-extra').eq(1).check().should('be.checked');
-    cy.contains('Check: {row : 1, value : TRUE}');
+    cy.contains('Check: {row : 2, value : TRUE, column : Check');
   });
 })
 
@@ -21,7 +21,7 @@ describe('Date in reactable passes values to the shiny', () => {
   it('Date', () => {
     cy.visit('http://localhost:8888');
     cy.get('.date-extra').eq(1).type('2020-01-01').should('have.value', '2020-01-01');
-    cy.contains('Date: {row : 1, value : 2020-01-01}');
+    cy.contains('Date: {row : 2, value : 2020-01-01, column : Date}');
   });
 })
 
