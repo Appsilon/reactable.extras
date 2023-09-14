d5695ee4441113a883c39bca909466327276423e
andres <afquinteromoreano@gmail.com>
2023-09-14 07:34:26 -0500

    chore: Fixes spelling

--- R/tooltip.R
+++ R/tooltip.R
@@ -1,11 +1,11 @@
-#' Tooltip for table headers
+#' Tool-tip for table headers
 #'
-#' @param content The content to be displayed in the tooltip
-#' @param theme The theme of the tooltip, either "light", "light-border", "material" or
+#' @param content The content to be displayed in the tool-tip
+#' @param theme The theme of the tool-tip, either "light", "light-border", "material" or
 #'   "translucent"
 #'
 #' @examples
-#' reactable::colDef(header = tooltip_extra("This is my tooltip", theme = "material"))
+#' reactable::colDef(header = tooltip_extra("This is my tool-tip", theme = "material"))
 #'
 #' @export
 tooltip_extra <- function(content, theme = "light") {
